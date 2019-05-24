c29dd24c475350537d0941278738b0394283e930
DEWAELE Matthieu <matthieu.dewaele@viacesi.fr>
2019-05-24 16:36:32 +0200

    :construction: adding Board and Level

--- entity/src/main/java/entity/Board.java
+++ entity/src/main/java/entity/Board.java
@@ -1,5 +1,31 @@
 package entity;
 
 public class Board {
-
+	private int width; 
+	private int height;
+	
+	public Board(int widht, int height) {
+		this.setWidth();
+		this.setHeight();
+	}
+	
+	public Board() {
+		
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public void setWidth() {
+		
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public void setHeight() {
+		
+	}
 }
--- entity/src/main/java/entity/Level.java
+++ entity/src/main/java/entity/Level.java
@@ -1,5 +1,33 @@
 package entity;
 
-public class Level {
+import java.util.ArrayList;
 
+public class Level extends Entity{
+	private Board board;
+	private ArrayList<ActiveEntity> pawns;
+	
+	public Level () {
+		board = new Board();
+	}
+	
+	public Board getBoard() {
+		return board;
+	}
+	
+	private void setBoard(Board board) {
+		this.board = board;
+	}
+	
+	public ArrayList<ActiveEntity> getPawns(){
+		return pawns;
+	}
+	
+	public void putPawn(ActiveEntity pawn) {
+		pawns.add(pawn);
+	}
+	
+	public void popPawn(ActiveEntity pawn) {
+		pawns.remove(pawn);
+	}
+	
 }
