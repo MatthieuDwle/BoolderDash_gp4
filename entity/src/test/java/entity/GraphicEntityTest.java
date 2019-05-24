f392aec950bbd1da1408d95a8ade78ff1e9d15c7
SamiKr <44946069+SamiKr@users.noreply.github.com>
2019-05-24 15:57:32 +0200

    :construction: adding class GaphicEntityTest

---
+++ entity/src/test/java/entity/GraphicEntityTest.java
@@ -0,0 +1,49 @@
+package entity;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class GraphicEntityTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGraphicEntity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetImage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetImage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoadImage() {
+		fail("Not yet implemented");
+	}
+
+}
