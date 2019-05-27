4f77dbe5ccca029f3684892f5e7676229f89ed1f
SamiKr <44946069+SamiKr@users.noreply.github.com>
2019-05-27 11:30:32 +0200

    :construction: Crystal without move() and pop()

---
+++ entity/src/main/java/entity/Crystal.java
@@ -0,0 +1,20 @@
+package entity;
+
+public class Crystal extends ActiveEntity implements IPop,IMovable{
+
+	public Crystal(String imageName) {
+		super(imageName);
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public void move(int direction) {
+		// TODO Auto-generated method stub
+		
+		}
+	
+	@Override
+	public void pop() {
+		// TODO Auto-generated method stub
+		
+	}
+}
