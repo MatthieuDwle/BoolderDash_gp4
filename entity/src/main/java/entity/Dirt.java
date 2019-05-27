cd7e49eeb2ebe952b9eaaae94b1079c5b2806a3a
SamiKr <44946069+SamiKr@users.noreply.github.com>
2019-05-27 11:31:02 +0200

    :construction: Dirt

---
+++ entity/src/main/java/entity/Dirt.java
@@ -0,0 +1,17 @@
+package entity;
+
+public class Dirt extends ActiveEntity implements IPop{
+
+	public Dirt(String imageName) {
+		super(imageName);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void pop() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
