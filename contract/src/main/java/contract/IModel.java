9cc8ac44a760e34327b7d8fdd36bb4a286379f6b
SamiKr <44946069+SamiKr@users.noreply.github.com>
2019-05-27 15:55:03 +0200

    :fire:

--- contract/src/main/java/contract/IModel.java
+++ contract/src/main/java/contract/IModel.java
@@ -2,7 +2,7 @@ package contract;
 
 import java.util.Observable;
 
-import entity.HelloWorld;
+
 
 /**
  * The Interface IModel.
@@ -10,22 +10,6 @@ import entity.HelloWorld;
  * @author Jean-Aymeric Diet
  */
 public interface IModel {
-
-	/**
-	 * Gets the hello world.
-	 *
-	 * @return the helloworld entity
-	 */
-	HelloWorld getHelloWorld();
-
-	/**
-	 * Load the message.
-	 *
-	 * @param code
-	 *          the code
-	 */
-	void loadHelloWorld(String code);
-
 	/**
 	 * Gets the observable.
 	 *
