afb8789ee084462f8d5abba114078463930feb5c
SamiKr <44946069+SamiKr@users.noreply.github.com>
2019-05-27 15:47:03 +0200

    :pencil: Java Doc

--- entity/src/main/java/entity/ActiveEntity.java
+++ entity/src/main/java/entity/ActiveEntity.java
@@ -3,28 +3,57 @@ package entity;
 import java.awt.Point;
 
 import fr.exia.showboard.IPawn;
-
+/**
+ * The Class View.
+ *
+ * @author Kosbur
+ */
 public class ActiveEntity extends GraphicEntity implements IPawn{
-	
+	/** The position. */
 	private Point position;
 
+	/**
+     * Instantiates a new ActiveEntity.
+     *
+     * @param imageName
+     *            the image name
+     */
 	public ActiveEntity(String imageName) {
 		super(imageName);
 		// TODO Auto-generated constructor stub
 	}
-
+	/**
+     * Sets the position.
+     *
+     * @param position
+     *            the position to set
+     */
 	public Point getPosition() {
 		return this.position;
 	}
-
+	  /**
+     * Sets the position.
+     *
+     * @param position
+     *            the position to set
+     */
 	private void setPosition(Point position) {
 		this.position = position;
 	}
 	
-	 
+	  /*
+     * (non-Javadoc)
+     * @see fr.exia.showboard.IPawn#getX()
+     */
+    @Override
 	   public int getX() {
 	        return this.getPosition().x;
 	    }
+    /*
+     * (non-Javadoc)
+     * @see fr.exia.showboard.IPawn#getY()
+     */
+    @Override
 	   public int getY() {
 	        return this.getPosition().y;
 	    }
