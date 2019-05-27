a1c132bdf94de053277e55a8bf5b21576b64c2af
DEWAELE Matthieu <matthieu.dewaele@viacesi.fr>
2019-05-27 17:53:01 +0200

    :construction: :bug: adding getLevel in IModel

--- contract/src/main/java/contract/IModel.java
+++ contract/src/main/java/contract/IModel.java
@@ -2,16 +2,20 @@ package contract;
 
 import java.util.Observable;
 
+import entity.Level;
+
 /**
  * The Interface IModel.
  *
  * @author Jean-Aymeric Diet
  */
-public interface IModel {
+public interface IModel{
 	/**
 	 * Gets the observable.
 	 *
 	 * @return the observable
 	 */
 	Observable getObservable();
+
+	Level getLevel();
 }
