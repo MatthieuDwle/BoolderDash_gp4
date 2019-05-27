b996bcb618e57ff78c576462b920b4f2bd458064
SamiKr <44946069+SamiKr@users.noreply.github.com>
2019-05-27 11:31:33 +0200

    :construction: Enemy, work in progress

---
+++ entity/src/main/java/entity/Enemy.java
@@ -0,0 +1,25 @@
+package entity;
+
+
+
+public class Enemy extends ActiveEntity implements IPop,IMovable{
+
+	private Object random;
+	public Enemy(String imageName) {
+		super(imageName);
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public void move(int direction) {
+		// TODO Auto-generated method stub
+		
+	}
+
+		@Override
+		public void pop() {
+			// TODO Auto-generated method stub
+		
+		}
+
+	
+}
