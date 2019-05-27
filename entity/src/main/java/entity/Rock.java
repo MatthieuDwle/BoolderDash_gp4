96f9ea8ebe370685c1a6840d1a7e0ba5f3d48d8c
SamiKr <44946069+SamiKr@users.noreply.github.com>
2019-05-27 11:33:35 +0200

    :construction: Rock without move()

---
+++ entity/src/main/java/entity/Rock.java
@@ -0,0 +1,14 @@
+package entity;
+
+public class Rock extends ActiveEntity implements IMovable{
+
+	public Rock(String imageName) {
+		super(imageName);
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public void move(int direction) {
+		// TODO Auto-generated method stub
+		
+	}
+}
