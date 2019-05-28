48d2ed635cec5f6abf9e48776d1023f53295cd0f
DEWAELE Matthieu <matthieu.dewaele@viacesi.fr>
2019-05-28 10:26:50 +0200

    :heavy_minus_sign: removing dependencies from ViewFrame

--- view/src/main/java/view/ViewFrame.java
+++ view/src/main/java/view/ViewFrame.java
@@ -1,15 +1,8 @@
 package view;
 
-import java.awt.GraphicsConfiguration;
 import java.awt.HeadlessException;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.ArrayList;
-import java.awt.Dimension;
-import java.awt.Rectangle;
-
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 
 import contract.IController;
 import contract.IModel;
