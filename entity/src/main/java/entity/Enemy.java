e8078f5aea92be337604bfef35b3b841b896f75e
SamiKr <44946069+SamiKr@users.noreply.github.com>
2019-05-28 09:37:10 +0200

    :rotating_light: Remove random attribute from Enemy and add new path in GraphicEntity

--- entity/src/main/java/entity/Enemy.java
+++ entity/src/main/java/entity/Enemy.java
@@ -4,7 +4,7 @@ package entity;
 
 public class Enemy extends ActiveEntity implements IPop,IMovable{
 
-	private Object random;
+	
 	public Enemy(String imageName) {
 		super(imageName);
 		// TODO Auto-generated constructor stub
