40b71934c7b9025c65665dfa08dfaffb2ea6562a
SamiKr <44946069+SamiKr@users.noreply.github.com>
2019-06-03 11:47:21 +0200

    :pencil::fire: Add JavaDoc for ActiveEntityTest, delete unusable methods

--- entity/src/test/java/entity/ActiveEntityTest.java
+++ entity/src/test/java/entity/ActiveEntityTest.java
@@ -2,34 +2,19 @@ package entity;
 
 import static org.junit.Assert.*;
 
-import java.awt.Point;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
+/**
+ * The ActiveEntity test class
+ * @author asus
+ *
+ */
 public class ActiveEntityTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
-
+	/**
+	 * Testing if GetX recover x
+	 */
 	@Test
 	public void testGetX() {
 		ActiveEntity X = new ActiveEntity("getX");
@@ -37,11 +22,13 @@ public class ActiveEntityTest {
 		assertEquals(4, X.getX());
 	}
 
+	/**
+	 * Testing if GetY recover y 
+	 */
 	@Test
 	public void testGetY() {
 		ActiveEntity Y = new ActiveEntity("getY");
 		Y.setPosition(0, 5);
 		assertEquals(5, Y.getY());
 	}
-
 }
